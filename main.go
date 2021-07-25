2aa5ace0aafe911cbdf5e3d2173ca35669a06a49
hidebon0630 <hyagi0630@icloud.com>
2021-07-25 19:11:39 +0900

    URLの国際化に対応する

--- main.go
+++ main.go
@@ -1,24 +1,16 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"net/http/httputil"
+	"fmt"
+
+	"golang.org/x/net/idna"
 )
 
 func main() {
-	client := &http.Client{}
-	request, err := http.NewRequest("DELETE", "http://localhost:18888", nil)
+	src := "握力王"
+	ascii, err := idna.ToASCII(src)
 	if err != nil {
 		panic(err)
 	}
-	resp, err := client.Do(request)
-	if err != nil {
-		panic(err)
-	}
-	dump, err := httputil.DumpResponse(resp, true)
-	if err != nil {
-		panic(err)
-	}
-	log.Println(string(dump))
+	fmt.Printf("%s -> %s\n", src, ascii)
 }
