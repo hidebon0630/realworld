ff633867a758dfec83ed269d0081a09448456286
hidebon0630 <hyagi0630@icloud.com>
2021-07-25 18:05:31 +0900

    multipart/form-data形式でファイルの送信

--- main.go
+++ main.go
@@ -1,14 +1,32 @@
 package main
 
 import (
+	"bytes"
+	"io"
 	"log"
+	"mime/multipart"
 	"net/http"
-	"strings"
+	"os"
 )
 
 func main() {
-	reader := strings.NewReader("テキスト")
-	resp, err := http.Post("http://localhost:18888", "text/plain", reader)
+	var buffer bytes.Buffer
+	writer := multipart.NewWriter(&buffer)
+	writer.WriteField("name", "Michael Jackson")
+	fileWriter, err := writer.CreateFormFile("thumbnail", "photo.jpg")
+	if err != nil {
+		panic(err)
+	}
+	readFile, err := os.Open("photo.jpg")
+	if err != nil {
+		// ファイル読み込み失敗
+		panic(err)
+	}
+	defer readFile.Close()
+	io.Copy(fileWriter, readFile)
+	writer.Close()
+
+	resp, err := http.Post("http://localhost:18888", writer.FormDataContentType(), &buffer)
 	if err != nil {
 		// 送信失敗
 		panic(err)
