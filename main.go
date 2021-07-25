a2543e4219272b41abf311612fa0401066cd681d
hidebon0630 <hyagi0630@icloud.com>
2021-07-25 18:33:49 +0900

    Transportを使ってプロキシを利用する

--- main.go
+++ main.go
@@ -3,27 +3,27 @@ package main
 import (
 	"log"
 	"net/http"
-	"net/http/cookiejar"
 	"net/http/httputil"
+	"net/url"
 )
 
 func main() {
-	jar, err := cookiejar.New(nil)
+	proxyUrl, err := url.Parse("http://localhost:18888")
 	if err != nil {
 		panic(err)
 	}
-	http.DefaultClient = &http.Client{
-		Jar: jar,
+	client := http.Client{
+		Transport: &http.Transport{
+			Proxy: http.ProxyURL(proxyUrl),
+		},
 	}
-	for i := 0; i < 2; i++ {
-		resp, err := http.Get("http://localhost:18888/cookie")
-		if err != nil {
-			panic(err)
-		}
-		dump, err := httputil.DumpResponse(resp, true)
-		if err != nil {
-			panic(err)
-		}
-		log.Println(string(dump))
+	resp, err := client.Get("http://github.com")
+	if err != nil {
+		panic(err)
 	}
+	dump, err := httputil.DumpResponse(resp, true)
+	if err != nil {
+		panic(err)
+	}
+	log.Println(string(dump))
 }
