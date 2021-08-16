767c060dbe2822d4217c3d892670ddeaf0a391ee
hidebon0630 <hyagi0630@icloud.com>
2021-08-16 19:14:47 +0900

    HTTP/2.0

--- main.go
+++ main.go
@@ -1,20 +1,15 @@
 package main
 
 import (
-	"crypto/sha1"
-	"encoding/base64"
 	"fmt"
+	"net/http"
 )
 
 func main() {
-	clientKeySrc := "the sample nonce"
-	key := base64.StdEncoding.EncodeToString([]byte(clientKeySrc))
-	fmt.Printf("Sec-WebSocket-Key %s\n", key)
-	// Sec-WebSocket-Key dGhlIHNhbXBsZSBub25jZQ==
-
-	salt := "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
-	hash := sha1.Sum([]byte(key + salt))
-	accept := base64.StdEncoding.EncodeToString(hash[:])
-	fmt.Printf("Sec-WebSocket-Accept: %s\n", accept)
-	// Sec-WebSocket-Accept: s3pPLMBiTxaQ9kYGzzhZRbK+xOo=
+	resp, err := http.Get("https://google.com/")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	fmt.Printf("Protocol Version: %s\n", resp.Proto)
 }
